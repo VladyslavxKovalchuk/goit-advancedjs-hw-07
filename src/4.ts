227dfbad86fc0004d6683ff2afeef922f6a9b6a9
Vladyslav Kovalchuk <vladkovalchuk09@gmail.com>
2025-01-19 19:07:21 +0200

    Тема 11. ООП і TypeScript. Домашня робота

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+  private signature: number = Math.random();
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  public getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  private tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Welcome to home. The door is open to you.");
+    } else {
+      console.log("The door is locked tight! Try again when it's open.");
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (this.door) {
+      console.log(
+        "The door is already open. You can step inside anytime!"
+      );
+    } else {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log("*CLICK* The door swings open with a magical *whoosh*!");
+      } else {
+        console.log("Wrong key! The secrets of the house remain hidden.");
+      }
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
